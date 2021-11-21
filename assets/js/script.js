1f98a51a57ce84f8afafe5b8b9f536b711f662a2
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-21 15:11:32 -0600

    Edit Some thing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 var inputValue = $('.search');
-var searchBtn = $(".searchBtn");
-var main = $("main");
+var searchBtn = $('.searchBtn');
+var storedSearches = $('#storedSearches');
+var main = $('main');
+
 var searches = [];
 
+//renders the searches
+function renderSearches() {
 
+    storedSearches.html("");
+    for (var i = 0; i < searches.length; i++) {
+        var search = searches[i];
 
+        var button = $('<button>');
+        button.text(search);
+        button.attr('data-index', i);
+        button.attr('class', 'list-group-item list-group-item-action');
 
-var fetchWeather= function (){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValue.val() + '&appid=c0c8e3bb6abc081507f7c5a91f54d2e0').then(function(respone){
-        respone.json().then(function(data){
-            console.log(data);
+        storedSearches.append(button);
+
+        button.on('click', function() {
+            inputValue.val(searches);
+            var citySearch = $(this).text()
+            inputValue.val(citySearch);
+            fetchWeather();
         })
-    })
+    }
 }
-
-
-
-searchBtn.on('click',fetchWeather);
-
